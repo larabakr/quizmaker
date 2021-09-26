544ba467e4e695645ddd1117d79821af354ae553
uwuxia <uwuxia@uwuxias-MacBook-Air.local>
2021-09-27 00:43:14 +0300

    added quiz search functionality and improved styling

--- src/components/quizzes.tsx
+++ src/components/quizzes.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class Quizzes extends React.Component {
+interface StateTypes {
+  quizzes: {
+    questions: {
+      question: string;
+      correctAnswer: string;
+      choices: string[];
+    }[][];
+    totalQuestions: number;
+    name: string;
+    id: string;
+  }[];
+  isLoading: boolean;
+}
+
+class Quizzes extends React.Component<{}, StateTypes> {
   state = {
     quizzes: [],
     isLoading: true,
@@ -15,11 +29,59 @@ class Quizzes extends React.Component {
       isLoading: false,
       quizzes: response,
     });
+
+    document.addEventListener('keypress', (e) => {
+      if (e.key === '/') {
+        setTimeout(() => document.getElementById('search')?.focus(), 0);
+      }
+    })
   }
+
+  search = (e: any) => {
+    if (e.key === "Enter") {
+      if (e.target.value === '') {
+        (async () => {
+          const data = await fetch("/quizzes");
+          const response = await data.json();
+  
+          this.setState({
+            quizzes: response,
+          });
+        })();
+      } else {
+        this.setState({
+          quizzes: this.state.quizzes.filter((quiz: any) =>
+            quiz.name.includes(e.target.value)
+          ),
+        });
+      }
+    }
+  };
+
+  restore = (e: any) => {
+    if (e.target.value === "") {
+      (async () => {
+        const data = await fetch("/quizzes");
+        const response = await data.json();
+
+        this.setState({
+          quizzes: response,
+        });
+      })();
+    } else return;
+  };
 
   render() {
     return (
       <div className="quizzes">
+        <input
+          type="text"
+          id="search"
+          placeholder="search..."
+          autoComplete="off"
+          onFocus={this.restore}
+          onKeyPress={this.search}
+        />
         {this.state.isLoading ? (
           <p>Loading...</p>
         ) : this.state.quizzes.length >= 1 ? (
